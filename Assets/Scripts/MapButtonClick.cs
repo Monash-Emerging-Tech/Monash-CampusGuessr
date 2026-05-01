86499698af3fd4cb257db2f861b947a26af2d05a
Rohan Shetty <rshe0040@student.monash.edu>
2026-05-01 12:27:16 +1000

    fix bug where game loads the initial map pack selected, not the most recent one

--- Assets/Scripts/MapButtonClick.cs
+++ Assets/Scripts/MapButtonClick.cs
@@ -2,16 +2,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// 
 /// Written by rohan0221
 /// Last Modified: 23/04/2026
 /// </summary>
 public class MapButtonClick : MonoBehaviour
 {
     public string targetScene;
+    public string mapPackName;
 
     void OnMouseDown()
     {
+        if (!string.IsNullOrEmpty(mapPackName))
+            GameLogic.Instance.SetMapPackByName(mapPackName);
+        
         SceneManager.LoadScene(targetScene);
     }
 }
