666b7e494c4fdec284e1796d2e8215cfdeff452d
Munkhjin0517 <mbya0001@student.monash.edu>
2026-07-28 18:00:15 +1000

    Made the score going up sound more smooth
    Added alternative score up sound and made score up sound more smooth so it doesnt sound its cut out.

--- Assets/Scripts/Score/ScoreAnimator.cs
+++ Assets/Scripts/Score/ScoreAnimator.cs
@@ -33,7 +33,7 @@ public class ScoreAnimator : MonoBehaviour
         float duration = ScoreAnimationSettings.Duration;
         float time = 0f;
 
-        //AudioManager.Instance.PlayScoreSFX();
+        AudioManager.Instance?.PlayScoreSFX();
 
         while (time < duration)
         {
@@ -47,7 +47,7 @@ public class ScoreAnimator : MonoBehaviour
             yield return null;
         }
 
-        //AudioManager.Instance.StopScoreSFX();
+        AudioManager.Instance?.StopScoreSFX(0.7f);
         SetText(to);
     }
 
@@ -63,12 +63,12 @@ public class ScoreAnimator : MonoBehaviour
     }
 
 
-    //private void Start()
-    //{
-    //    Test();
-    //}
-    //public void Test()
-    //{
-    //    HandleScoreChanged(1500);
-    //}
+    private void Start()
+    {
+       Test();
+    }
+    public void Test()
+    {
+       HandleScoreChanged(1500);
+    }
 }
